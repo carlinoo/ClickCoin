336926910cd051a039e09e890c7064dd014030ac
Carlino <carlinoo@icloud.com>
2018-04-04 11:41:41 +0100

    added test

--- test/manual/dev-test.js
+++ test/manual/dev-test.js
@@ -1,12 +1,11 @@
-const Block = require('../../blockchain/block');
-// const block = new Block();
-const firstBlock = Block.genesis();
-const secondBlock = Block.mineBlock(firstBlock, 'second');
-const thirdBlock = Block.mineBlock(secondBlock, 'third');
-const fourthBlock = Block.mineBlock(thirdBlock, 'fourth');
+const Blockchain = require('../../blockchain');
+const bc = new Blockchain();
+let time = Date.now();
+let past_time = time;
 
-
-console.log(firstBlock.toString());
-console.log(secondBlock.toString());
-console.log(thirdBlock.toString());
-console.log(fourthBlock.toString());
+for (let i = 0; i < 10; i++) {
+  past_time = time;
+  time = Date.now();
+  console.log(bc.addBlock('foo ' + i).toString());
+  console.log("\nTIME TAKEN: " + (time - past_time)/1000 + "seconds\n");
+}
