00ab58bcef58b13aa61c5442af5674079e40b931
Carlino <carlinoo@icloud.com>
2018-04-05 21:57:49 +0100

    added Wallet Class

--- wallet/index.js
+++ wallet/index.js
@@ -1,8 +1,18 @@
+const CONFIG = require('../config');
 
-
-
+// This class represents the Wallet that will indicate the balance of it plus the keys
 class Wallet {
   constructor() {
-
+    this.balance = CONFIG["INITIAL_BALANCE"];
+    this.keyPair = null;
+    this.publicKey = null;
   }
+
+
+  // This method will display a wallet formatted properly
+  toString() {
+    return "Wallet -\n  - Balance:  " + this.balance + "\n  - publicKey:  " + this.publicKey.toString();
+  }
+
+
 }
