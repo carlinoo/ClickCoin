667bcfa810f39955186080ac5b7b0e4a0a9cbf89
Carlino <carlinoo@icloud.com>
2018-05-03 16:12:38 +0100

    added transaction

---
+++ wallet/transaction.js
@@ -0,0 +1,40 @@
+const ChainUtil = require('../chain-util');
+
+
+class Transaction {
+  constructor() {
+    this.id = ChainUtil.id();
+    this.input = null;
+    this.output = [];
+  }
+
+
+  // Static method to generate a new transaction object
+  static newTransaction(senderWallet, recipient, amount) {
+    const transaction = new this();
+
+    // Sender cannot send amount he doesnt have
+    if (amount > senderWallet.balance) {
+      console.log(`Amount ${amount} exceeds balance`);
+      return;
+    }
+
+    transaction.outputs.push(...[
+      {
+        amount: senderWallet.balance - amount,
+        address: senderWallet.publicKey
+      },
+      {
+        amount: amount,
+        address: recipient
+      }
+    ]);
+
+    return transaction;
+  }
+}
+
+
+
+
+module.exports = Transaction;
