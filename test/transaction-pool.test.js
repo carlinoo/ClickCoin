706040f7285db61f0536b34b13d43e26ebbb1ef7
Carlino Gonzalez <carlinoo@icloud.com>
2018-05-07 15:05:34 +0100

    test TransactionPool

---
+++ test/transaction-pool.test.js
@@ -0,0 +1,30 @@
+const TransactionPool = require("../wallet/transaction-pool");
+const Transaction = require("../wallet/transaction");
+const Wallet = require("../wallet");
+
+describe("TransactionPool", () => {
+  let tp, wallet, transaction;
+
+  beforeEach(() => {
+    tp = new TransactionPool();
+    wallet = new Wallet();
+    transaction = Transaction.newTransaction(wallet, 'r4nd-4ddr355', 30);
+    tp.updateOrAddTransaction(transaction);
+  });
+
+
+  // 
+  it('adds a transaction to the pool', () => {
+    expect(tp.transactions.find(t => t.id === transaction.id)).toEqual(transaction);
+  });
+
+
+  // 
+  it('updates a transaction in the pool', () => {
+    const oldTransaction = JSON.stringify(transaction);
+    const newTransaction = transaction.update(wallet, 'foo-4ddr355', 40);
+    tp.updateOrAddTransaction(newTransaction);
+
+    expect(JSON.stringify(tp.transactions.find(t => t.id === newTransaction.id))).not.toEqual(oldTransaction);
+  });
+});
