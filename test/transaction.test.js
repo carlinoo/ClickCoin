00aa0971a1043f7f1f955aeecddd5a64a2f573b4
Carlino Gonzalez <carlinoo@icloud.com>
2018-05-07 14:40:48 +0100

    updating transaction tests

--- test/transaction.test.js
+++ test/transaction.test.js
@@ -53,5 +53,28 @@ describe("Transaction", () => {
       expect(transaction).toEqual(undefined);
     });
   });
+
+
+
+
+  describe('and updating a transaction', () => {
+    let nextAmount, nextRecipient;
+
+    beforeEach(() => {
+      nextAmount = 20;
+      nextRecipient = 'n3xt-4ddr355';
+      transaction = transaction.update(wallet, nextRecipient, nextAmount);
+    });
+
+
+    it('subcracts the nextAmount from the sender\'s output', () => {
+      expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount).toEqual(wallet.balance - amount - nextAmount)
+    });
+
+
+    it('outputs an amount for the next recipient', () => {
+      expect(transaction.outputs.find(output => output.address === nextRecipient).amount).toEqual(nextAmount);
+    });
+  });
   
 });
